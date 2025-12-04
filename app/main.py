eb327591857589f6202bb898cd2a17bcdadd2fd9
sharath-mohan <sharathdesignsofficial@gmail.com>
2025-12-04 23:43:30 +0530

    gsi based pagination added

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI, Path
+from fastapi.params import Query
 from .schema import Item
-from .models import User
+from .models import User, Product
 from uuid import uuid4, UUID
 import time
 from typing import Annotated
@@ -10,6 +11,46 @@ app = FastAPI()
 # create a constant that has 100 fake users
 id = uuid4()
 users= [User(search_id=str(id), user_id =str(i),  name=f"User{i}", age=20 + i % 10, timestamp=int(time.thread_time_ns())) for i in range(5)]
+
+@app.get("/add-product")
+def add_products():
+    start_time = time.perf_counter()
+    id = uuid4()
+    products= [Product(search_id=str(id), product_id =str(i),  name=f"Product{i}" , timestamp=int(time.thread_time_ns()) ) for i in range(100)]
+    i=0
+    with Product.batch_write() as batch:
+       
+        for product in products:
+            i+=1
+            batch.save(product)
+            print(i)
+    end_time = time.perf_counter()
+    elapsed_time_secs = end_time - start_time
+    msg = f"Execution took: {timedelta(seconds=round(elapsed_time_secs))} (Wall clock time)"
+    print(msg)        
+    return {"message": len(products)}
+
+@app.get("/products/{search_id}")
+def read_products(search_id:Annotated[UUID,Path()], limit:Annotated[int,Query()]=10, offset:Annotated[int,Query()]=0):
+    results = []
+    start_time = time.perf_counter()
+    # query =  Product.query(str(search_id), limit=limit)
+    # items = [product.attribute_values for product in query]
+
+    # query = Product.view_index.query(str(search_id), Product.timestamp > offset, limit=limit)
+    # items = [product.attribute_values for product in query]
+    # return {"message": items}
+    index_results = list(Product.view_index.query(str(search_id), Product.timestamp > offset, limit=limit, last_evaluated_key=None))
+    keys_to_fetch = [(item.search_id, item.product_id) for item in index_results]
+    print(f"Keys to fetch: {keys_to_fetch}")
+    if keys_to_fetch:
+        for full_item in Product.batch_get(keys_to_fetch):
+            results.append(full_item.attribute_values)
+    end_time = time.perf_counter()
+    elapsed_time_secs = end_time - start_time
+    msg = f"Execution took: {timedelta(seconds=round(elapsed_time_secs))} (Wall clock time)"
+    print(msg)
+    return {"message": results, "lastIndex": index_results[-1].timestamp if index_results else None, "last_index_item": index_results[-1].attribute_values if index_results else None, "keys_to_fetch": keys_to_fetch}
 
 @app.post("/")
 def add_results(item:Item):
@@ -57,3 +98,18 @@ def count_results(search_id:Annotated[UUID,Path()]):
     msg = f"Execution took: {timedelta(seconds=round(elapsed_time_secs))} (Wall clock time)"
     print(msg)
     return {"message": count}
+
+
+
+@app.delete("/{search_id}")
+def delete_products(search_id:Annotated[UUID,Path()]):
+    start_time = time.perf_counter()
+    delete_count = 0
+    for product in Product.query(str(search_id)):
+        product.delete()
+        delete_count += 1
+    end_time = time.perf_counter()
+    elapsed_time_secs = end_time - start_time
+    msg = f"Execution took: {timedelta(seconds=round(elapsed_time_secs))} (Wall clock time)"
+    print(msg)        
+    return {"message": delete_count}
--- app/models.py
+++ app/models.py
@@ -1,17 +1,55 @@
 from pynamodb.models import Model
 from pynamodb.attributes import UnicodeAttribute, NumberAttribute, ListAttribute
 
+from pynamodb.indexes import GlobalSecondaryIndex, KeysOnlyProjection
+
 
 class User(Model):
     class Meta:
         table_name = "user_results"
         region = "us-east-1"
-        host = 'http://localhost:8000'
-        aws_access_key_id = 'cqmfac'
-        aws_secret_access_key = 'bi1w1'
+        host = "http://localhost:8000"
+        aws_access_key_id = "cqmfac"
+        aws_secret_access_key = "bi1w1"
+
     search_id = UnicodeAttribute(hash_key=True)
     user_id = UnicodeAttribute(range_key=True)
     name = UnicodeAttribute()
     age = NumberAttribute()
     timestamp = NumberAttribute()
     subject = ListAttribute(null=True)
+
+
+class ViewIndex(GlobalSecondaryIndex):
+    """
+    This class represents a global secondary index
+    """
+
+    class Meta:
+        # index_name is optional, but can be provided to override the default name
+        index_name = "timestamp_index"
+        read_capacity_units = 2
+        write_capacity_units = 1
+        # All attributes are projected
+        projection = KeysOnlyProjection()
+
+    # This attribute is the hash key for the index
+    # Note that this attribute must also exist
+    # in the model
+    search_id = UnicodeAttribute(hash_key=True)
+    timestamp = NumberAttribute(range_key=True)
+
+
+class Product(Model):
+    class Meta:
+        table_name = "product_results"
+        region = "us-east-1"
+        host = "http://localhost:8000"
+        aws_access_key_id = "cqmfac"
+        aws_secret_access_key = "bi1w1"
+
+    search_id = UnicodeAttribute(hash_key=True)
+    product_id = UnicodeAttribute(range_key=True)
+    name = UnicodeAttribute()
+    timestamp = NumberAttribute()
+    view_index = ViewIndex()
